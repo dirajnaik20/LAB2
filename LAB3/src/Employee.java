0d2fbf7b2141563d97b539788da4b25bb939375a
admin <admin@Bcalab3-m06>
2022-03-16 10:27:10 +0530

    Commitnew2

---
+++ LAB3/src/Employee.java
@@ -0,0 +1,18 @@
+
+public class Employee {
+	
+	String ename;
+	
+	Employee(String ename)
+	{
+		this.ename=ename;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee E1=new Employee("Diraj");
+
+	}
+
+}
